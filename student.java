dacf3ac3a2f4a005ec02e9481372b0576d170d48
Soumya <soumyareddy165@gmail.com>
2021-11-01 06:29:57 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,22 @@
+public class Student{
+
+  private int id;
+  private String name;
+  private int year;
+
+  public Student(int id1, String name1, int year1)
+  {
+    id = id1;
+    name = name1;
+    year = year1;
+  }
+
+  public Student()
+  {
+  }
+
+  public int getyear()
+  {
+    return year;
+  }
+}
