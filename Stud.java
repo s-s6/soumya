85b924b5dc5e5fae2994b719ba620a04c5c20d1d
Soumya <soumyareddy165@gmail.com>
2021-11-02 06:25:42 +0530

    Create Stud.java

---
+++ Stud.java
@@ -0,0 +1,22 @@
+public class Stud{
+  private int admission;
+  private String course;
+  private int roll_no;
+
+  public students(int years, String courses, int rollnos){
+    admission = years;
+    course = courses;
+    roll_no = rollnos;
+  }
+
+  public String getunique_id(int year){
+    String unique_id1 = year + course + roll_no;
+    return unique_id1;
+  }
+
+  public String getunique_id(){
+    String unique_id2 = roll_no + course + admission;
+    return unique_id2;
+  }
+
+}
