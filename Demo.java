b7556e71642e837e1a0986296166d6034c9da468
Soumya <soumyareddy165@gmail.com>
2021-11-02 06:42:56 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
 public class Demo{
   public static void main(String args[]){
     System.out.println("Hello everyone ");
-    students s1 = new students(2015, "AIM", 109);
-    students s2 = new students(2006, "CSE", 44);
+    Students s1 = new Students(2015, "AIM", 109);
+    Students s2 = new Students(2006, "CSE", 44);
     String id1 = s1.getunique_id(2015);
     System.out.println("Unique ID for admissison from or after 2015 - " + id1);
     String id2 = s2.getunique_id();
